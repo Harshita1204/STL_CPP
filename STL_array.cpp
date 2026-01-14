36e2c88fb6bc271f53bc769c7c310e87b1adb785
Harshita1204 <harshitak1204@gmail.com>
2026-01-14 20:08:01 +0530

    Create STL_array.cpp

---
+++ STL_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() {
+    array<int, 5> a = {1, 2, 3, 4, 5};
+
+    cout << a.size() << endl;     // 5
+    cout << a.front() << endl;   // 1
+    cout << a.back() << endl;    // 5
+
+    a.fill(10);
+    for (int x : a) cout << x << " ";
+    // Output: 10 10 10 10 10
+}
